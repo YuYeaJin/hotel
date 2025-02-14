5fa41991bd13e022e73762246ce99ced694a8035
junil3005 <yjl3005@naver.com>
2025-02-14 13:15:01 +0900

    [준일]호텔Dao

--- src/main/java/hotelproject/repositories/HotelDao.java
+++ src/main/java/hotelproject/repositories/HotelDao.java
@@ -1,5 +1,12 @@
 package hotelproject.repositories;
 
-public interface HotelDao {
+import hotelproject.repositories.vo.HotelVo;
+import java.util.List;
 
+public interface HotelDao {
+    List<HotelVo> getAllHotels();	//	호텔조회
+    HotelVo getHotelById(int hotelId);	//	호텔아이디
+    void addHotel(HotelVo hotel);	//	호텔추가
+    void updateHotel(HotelVo hotel);	//	호텔수정
+    void deleteHotel(int hotelId);	//	호텔삭제
 }
